eae6e7f5923125dde38957b6926bb51595924541
tristan <tsmackinlay@gmail.com>
2016-07-02 18:19:52 -0700

    Set up index
    -made basic index with search form.
    
    -configured search form with api to return a list of matching cities.

---
+++ little_dress_report/application.js
@@ -0,0 +1,25 @@
+
+$(function() {
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+    $.ajax({
+      url: 'http://autocomplete.wunderground.com/aq',
+      dataType: 'jsonp',
+      jsonp: 'cb',
+      data: {
+        query: $('input[name=query]').val(),
+        format: 'json',
+      },
+      success: function(response) {
+        var results = response.RESULTS;
+        $.each(results, function(i, result) {
+          console.log(result.name);
+          $('div#results').append($('<p class="result">').text(result.name));
+        });
+      }
+    });
+  });
+
+
+});
