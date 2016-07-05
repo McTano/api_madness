d91190520d1b42a7a8a2be028fb70b59e5d04ba1
tristan <tsmackinlay@gmail.com>
2016-07-05 13:54:24 -0700

    ldr now checks weather and returns an answer.

--- little_dress_report/application.js
+++ little_dress_report/application.js
@@ -1,5 +1,10 @@
+"use strict";
+var api_key = '6f5df01d0f316976';
+
 
 $(function() {
+  var $query_box = $('input.query');
+  var $results = $('ul.results');
 
   $('form').on('submit', function(event) {
     event.preventDefault();
@@ -8,15 +13,40 @@ $(function() {
       dataType: 'jsonp',
       jsonp: 'cb',
       data: {
-        query: $('input[name=query]').val(),
+        query: $query_box.val(),
         format: 'json',
       },
       success: function(response) {
+        $results.html('');
         var results = response.RESULTS;
         $.each(results, function(i, result) {
-          console.log(result.name);
-          $('ul.results').append($('<li class="result">').text(result.name));
+          var $list_result = $('<li>');
+          $list_result.addClass('result');
+          $list_result.text(result.name);
+          $list_result.data('link', result.l);
+          $results.append($list_result);
         });
+      }
+    });
+  });
+
+  $results.on('click', 'li.result', function() {
+    var city = $(this).text();
+    $query_box.val(city);
+    var link = $(this).data('link');
+    var url = 'http://api.wunderground.com/api/' + api_key + '/forecast' + link + '.json';
+    $.ajax({
+      url: url,
+      success: function(response) {
+        var forecast = response.forecast.simpleforecast.forecastday[0];
+        var low = forecast.low.celsius;
+        console.log(low);
+        if (low >= 20) {
+          $results.text('YES!');
+        }
+        else {
+          $results.text("I'm not going to tell you what to do, but it's going to be " + low + " degrees out later, so bring a sweater.")
+        }
       }
     });
   });
