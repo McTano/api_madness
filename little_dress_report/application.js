9fa2ac0ec42dedadf5842232a12e1970fdbfd836
tristan <tsmackinlay@gmail.com>
2016-07-11 16:46:35 -0700

    used this project as testing ground for eventful api

--- little_dress_report/application.js
+++ little_dress_report/application.js
@@ -9,47 +9,47 @@ $(function() {
   $('form').on('submit', function(event) {
     event.preventDefault();
     $.ajax({
-      url: 'http://autocomplete.wunderground.com/aq',
+      url: 'http://api.eventful.com/json/events/search',
       dataType: 'jsonp',
-      jsonp: 'cb',
       data: {
-        query: $query_box.val(),
-        format: 'json',
+        location: $query_box.val(),
+        app_key: 'FFmssWtvRRfc9VF7',
       },
       success: function(response) {
         $results.html('');
-        var results = response.RESULTS;
+        var results = response.events.event;
+        console.log(results);
         $.each(results, function(i, result) {
           var $list_result = $('<li>');
           $list_result.addClass('result');
-          $list_result.text(result.name);
-          $list_result.data('link', result.l);
+          $list_result.text(result.title);
+          // $list_result.data('link', result.l);
           $results.append($list_result);
         });
       }
     });
   });
 
-  $results.on('click', 'li.result', function() {
-    var city = $(this).text();
-    $query_box.val(city);
-    var link = $(this).data('link');
-    var url = 'http://api.wunderground.com/api/' + api_key + '/forecast' + link + '.json';
-    $.ajax({
-      url: url,
-      success: function(response) {
-        var forecast = response.forecast.simpleforecast.forecastday[0];
-        var low = forecast.low.celsius;
-        console.log(low);
-        if (low >= 20) {
-          $results.text('YES!');
-        }
-        else {
-          $results.text("I'm not going to tell you what to do, but it's going to be " + low + " degrees out later, so bring a sweater.")
-        }
-      }
-    });
-  });
+  // $results.on('click', 'li.result', function() {
+  //   var city = $(this).text();
+  //   $query_box.val(city);
+  //   var link = $(this).data('link');
+  //   var url = 'http://api.wunderground.com/api/' + api_key + '/forecast' + link + '.json';
+  //   $.ajax({
+  //     url: url,
+  //     success: function(response) {
+  //       var forecast = response.forecast.simpleforecast.forecastday[0];
+  //       var low = forecast.low.celsius;
+  //       console.log(low);
+  //       if (low >= 20) {
+  //         $results.text('YES!');
+  //       }
+  //       else {
+  //         $results.text("I'm not going to tell you what to do, but it's going to be " + low + " degrees out later, so bring a sweater.")
+  //       }
+  //     }
+  //   });
+  // });
 
 
 });
